145082362a83f46017d839ab6d924251bba0228f
Sam Platt <samuel.j.platt@gmail.com>
2015-06-24 23:26:31 -0400

    First changes for searchkick

--- app/controllers/dlcs_controller.rb
+++ app/controllers/dlcs_controller.rb
@@ -1,13 +1,13 @@
 class DlcsController < ApplicationController
   def index
     if params[:query].present?
-      @search_results = Dlc.search(params[:query], page: params[:page])
+      @search_results = Dlc.search(params[:query],fields: [:name],page: params[:page])
     else
       @search_results = Dlc.all.page params[:page]
     end
   end
 
   def autocomplete
-    render json: Dlc.search(params[:query], autocomplete: true, limit: 10).map(&:name)
+    render json: Dlc.search(params[:query], misspellings: {distance: 1}, highlight: false,partial: true, fields: [{name: :word_start}], limit: 10).map(&:name)
   end
 end
