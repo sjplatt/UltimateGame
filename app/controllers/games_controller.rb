1572bea1bdf82ba6fa8ce1022334c6a4ce9b592d
Sam Platt <samuel.j.platt@gmail.com>
2015-06-25 20:38:35 -0400

    Change Game Controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,13 @@
 class GamesController < ApplicationController
   def index
     if params[:query].present?
-      @search_results = Game.search(params[:query], page: params[:page])
+      @search_results = Game.search(params[:query],fields: [:name],page: params[:page])
     else
       @search_results = Game.all.page params[:page]
     end
   end
 
   def autocomplete
-    render json: Game.search(params[:query], autocomplete: true, limit: 10).map(&:name)
+    render json: Game.search(params[:query], misspellings: {distance: 1}, highlight: false,partial: true, fields: [{name: :word_start}], limit: 10).map(&:name)
   end
 end
