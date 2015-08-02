edb2d82b77e813ee4aa9d3a66603f35da65a5941
Matthew Zhu <matt.z496@gmail.com>
2015-08-02 17:14:34 -0400

    Code cleanup/decomposition

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( get_game/get_game_extra_formatting.js )
+Rails.application.config.assets.precompile += %w( get_game/prices.js )
+Rails.application.config.assets.precompile += %w( get_game/images.js )
+Rails.application.config.assets.precompile += %w( get_game/videos.js )
+Rails.application.config.assets.precompile += %w( get_game/reddit.js )
+Rails.application.config.assets.precompile += %w( get_game/search_results.js )
+Rails.application.config.assets.precompile += %w( get_game/footer.js )
